86f270ce33ae26172136115325606871ddb5931e
zhangxiaofengjs <30919487+zhangxiaofengjs@users.noreply.github.com>
2017-08-11 04:55:22 +0000

    add

---
+++ src/strawberry/strawberry.client/ISBFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace strawberry.client
+{
+	public interface ISBFile
+	{
+		string Name { get; set; }
+		string Path { get; set; }
+		long Size { get; set; }//byte
+		Image Icon { get; set; }
+	}
+}
---
+++ src/strawberry/strawberry.cmn/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace strawberry.cmn
+{
+	public class Class1
+	{
+		public static void ado()
+		{
+		}
+	}
+}
