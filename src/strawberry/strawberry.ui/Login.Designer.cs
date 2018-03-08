cb0162d39de70d820ef9e38d1af4a4d1924cb0b2
ABCDEFGHJKMN <30920014+ABCDEFGHJKMN@users.noreply.github.com>
2018-03-08 16:39:57 +0800

    login

---
+++ src/strawberry/strawberry.ui/Login.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace strawberry.ui
+{
+	partial class Login
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox_UserName = new System.Windows.Forms.TextBox();
+			this.textBox_PassWord = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.label1.Location = new System.Drawing.Point(33, 64);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(62, 12);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "User Name";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.label2.Location = new System.Drawing.Point(35, 120);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(59, 12);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Pass Word";
+			// 
+			// textBox_UserName
+			// 
+			this.textBox_UserName.Location = new System.Drawing.Point(151, 64);
+			this.textBox_UserName.Name = "textBox_UserName";
+			this.textBox_UserName.Size = new System.Drawing.Size(100, 19);
+			this.textBox_UserName.TabIndex = 2;
+			this.textBox_UserName.TextChanged += new System.EventHandler(this.textBox_UserName_TextChanged);
+			// 
+			// textBox_PassWord
+			// 
+			this.textBox_PassWord.Location = new System.Drawing.Point(151, 112);
+			this.textBox_PassWord.Name = "textBox_PassWord";
+			this.textBox_PassWord.Size = new System.Drawing.Size(100, 19);
+			this.textBox_PassWord.TabIndex = 3;
+			// 
+			// Login
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.textBox_PassWord);
+			this.Controls.Add(this.textBox_UserName);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Login";
+			this.Text = "Login";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox_UserName;
+		private System.Windows.Forms.TextBox textBox_PassWord;
+	}
+}
