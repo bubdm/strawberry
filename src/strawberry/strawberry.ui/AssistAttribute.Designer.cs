b6d2c7f51b1a344b44b1b40697577d3278e5ff1a
WAGMM <30920131+WAGMM@users.noreply.github.com>
2018-03-20 14:27:43 +0800

    属性

---
+++ src/strawberry/strawberry.ui/AssistAttribute.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace strawberry.ui
+{
+	partial class AssistAttribute
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.label13 = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			this.label15 = new System.Windows.Forms.Label();
+			this.label16 = new System.Windows.Forms.Label();
+			this.label17 = new System.Windows.Forms.Label();
+			this.label18 = new System.Windows.Forms.Label();
+			this.label19 = new System.Windows.Forms.Label();
+			this.label20 = new System.Windows.Forms.Label();
+			this.label21 = new System.Windows.Forms.Label();
+			this.label22 = new System.Windows.Forms.Label();
+			this.label23 = new System.Windows.Forms.Label();
+			this.label24 = new System.Windows.Forms.Label();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Location = new System.Drawing.Point(12, 12);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(248, 238);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.label13);
+			this.tabPage1.Controls.Add(this.label14);
+			this.tabPage1.Controls.Add(this.label15);
+			this.tabPage1.Controls.Add(this.label16);
+			this.tabPage1.Controls.Add(this.label17);
+			this.tabPage1.Controls.Add(this.label18);
+			this.tabPage1.Controls.Add(this.label19);
+			this.tabPage1.Controls.Add(this.label20);
+			this.tabPage1.Controls.Add(this.label21);
+			this.tabPage1.Controls.Add(this.label22);
+			this.tabPage1.Controls.Add(this.label23);
+			this.tabPage1.Controls.Add(this.label24);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(240, 212);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "全般";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.Location = new System.Drawing.Point(103, 117);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(41, 12);
+			this.label13.TabIndex = 23;
+			this.label13.Text = "label13";
+			// 
+			// label14
+			// 
+			this.label14.AutoSize = true;
+			this.label14.Location = new System.Drawing.Point(103, 97);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(41, 12);
+			this.label14.TabIndex = 22;
+			this.label14.Text = "label14";
+			// 
+			// label15
+			// 
+			this.label15.AutoSize = true;
+			this.label15.Location = new System.Drawing.Point(103, 77);
+			this.label15.Name = "label15";
+			this.label15.Size = new System.Drawing.Size(41, 12);
+			this.label15.TabIndex = 21;
+			this.label15.Text = "label15";
+			// 
+			// label16
+			// 
+			this.label16.AutoSize = true;
+			this.label16.Location = new System.Drawing.Point(103, 57);
+			this.label16.Name = "label16";
+			this.label16.Size = new System.Drawing.Size(41, 12);
+			this.label16.TabIndex = 20;
+			this.label16.Text = "label16";
+			// 
+			// label17
+			// 
+			this.label17.AutoSize = true;
+			this.label17.Location = new System.Drawing.Point(103, 37);
+			this.label17.Name = "label17";
+			this.label17.Size = new System.Drawing.Size(41, 12);
+			this.label17.TabIndex = 19;
+			this.label17.Text = "label17";
+			// 
+			// label18
+			// 
+			this.label18.AutoSize = true;
+			this.label18.Location = new System.Drawing.Point(103, 17);
+			this.label18.Name = "label18";
+			this.label18.Size = new System.Drawing.Size(41, 12);
+			this.label18.TabIndex = 18;
+			this.label18.Text = "label18";
+			// 
+			// label19
+			// 
+			this.label19.AutoSize = true;
+			this.label19.Location = new System.Drawing.Point(5, 117);
+			this.label19.Name = "label19";
+			this.label19.Size = new System.Drawing.Size(41, 12);
+			this.label19.TabIndex = 17;
+			this.label19.Text = "更新日";
+			// 
+			// label20
+			// 
+			this.label20.AutoSize = true;
+			this.label20.Location = new System.Drawing.Point(6, 97);
+			this.label20.Name = "label20";
+			this.label20.Size = new System.Drawing.Size(41, 12);
+			this.label20.TabIndex = 16;
+			this.label20.Text = "更新者";
+			// 
+			// label21
+			// 
+			this.label21.AutoSize = true;
+			this.label21.Location = new System.Drawing.Point(6, 77);
+			this.label21.Name = "label21";
+			this.label21.Size = new System.Drawing.Size(41, 12);
+			this.label21.TabIndex = 15;
+			this.label21.Text = "作成日";
+			// 
+			// label22
+			// 
+			this.label22.AutoSize = true;
+			this.label22.Location = new System.Drawing.Point(6, 57);
+			this.label22.Name = "label22";
+			this.label22.Size = new System.Drawing.Size(41, 12);
+			this.label22.TabIndex = 14;
+			this.label22.Text = "作成者";
+			// 
+			// label23
+			// 
+			this.label23.AutoSize = true;
+			this.label23.Location = new System.Drawing.Point(6, 37);
+			this.label23.Name = "label23";
+			this.label23.Size = new System.Drawing.Size(34, 12);
+			this.label23.TabIndex = 13;
+			this.label23.Text = "サイズ";
+			// 
+			// label24
+			// 
+			this.label24.AutoSize = true;
+			this.label24.Location = new System.Drawing.Point(6, 17);
+			this.label24.Name = "label24";
+			this.label24.Size = new System.Drawing.Size(51, 12);
+			this.label24.TabIndex = 12;
+			this.label24.Text = "ファイル名";
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(240, 212);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "以前のバージョン";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// AssistAttribute
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.tabControl1);
+			this.Name = "AssistAttribute";
+			this.Text = "属性";
+			this.Load += new System.EventHandler(this.AssistAttribute_Load);
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.Label label15;
+		private System.Windows.Forms.Label label16;
+		private System.Windows.Forms.Label label17;
+		private System.Windows.Forms.Label label18;
+		private System.Windows.Forms.Label label19;
+		private System.Windows.Forms.Label label20;
+		private System.Windows.Forms.Label label21;
+		private System.Windows.Forms.Label label22;
+		private System.Windows.Forms.Label label23;
+		private System.Windows.Forms.Label label24;
+	}
+}
---
+++ src/strawberry/strawberry.ui/AssistAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Windows.Forms;
+using strawberry.ui;
+using System.Collections;
+using strawberry.lanfolder;
+using strawberry.client;
+using Microsoft.VisualBasic;
+
+namespace strawberry.ui
+{
+	public partial class AssistAttribute : Form
+	{
+		public AssistAttribute()
+		{
+			InitializeComponent();
+		}
+		public void Init()
+		{
+			FormMain frmMain = new FormMain();
+			ListViewItem listViewItem = frmMain.listViewSelectedItem;
+			string path = listViewItem.Tag.ToString();
+			label18.Text = path.Substring(path.LastIndexOf("\\") + 1);
+			label17.Text = listViewItem.SubItems[5].Text + "KB";
+			label16.Text = "creator";
+			label15.Text = File.GetCreationTime(path).ToString();
+			label14.Text = "updater";
+			label13.Text = File.GetLastWriteTime(path).ToString();
+		}
+
+		private void AssistAttribute_Load(object sender, EventArgs e)
+		{
+			Init();
+		}
+	}
+}
